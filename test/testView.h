f07394610f3d6c48e201ba45cee8bfde14396a7e
Yuta Kinashi <y.nashiki@gmail.com>
2015-08-31 23:47:08 +0900

    saver

---
+++ test/testView.h
@@ -0,0 +1,17 @@
+//
+//  testView.h
+//  test
+//
+//  Created by Yuta Kinashi on 2015/08/31.
+//  Copyright (c) 2015年 Yuta Kinashi. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+#import <WebKit/WebKit.h>
+
+@interface testView : ScreenSaverView
+{
+    WebView *webView;
+}
+
+@end
